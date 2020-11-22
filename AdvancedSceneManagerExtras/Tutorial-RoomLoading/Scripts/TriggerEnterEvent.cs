f838ede1c62aea44b100bacad23fc330e709bc5f
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-11-22 10:48:11 +0100

    Pushed tutorial

---
+++ AdvancedSceneManagerExtras/Tutorial-RoomLoading/Scripts/TriggerEnterEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEnterEvent : MonoBehaviour
+{
+
+    public string _tag = "Player";
+
+    public UnityEvent OnEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(_tag))
+            OnEnter.Invoke();
+    }
+
+}
